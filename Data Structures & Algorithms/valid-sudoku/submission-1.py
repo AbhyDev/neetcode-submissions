c113332c009f3f80aa523257eb323ed4c7119961
AbhyuDev <153921525+AbhyDev@users.noreply.github.com>
2026-08-30 00:28:51 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.py
@@ -0,0 +1,43 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        m=9
+        n=9
+        def func(board,i,j):
+            s=set()
+            m=9
+            n=9
+            arrx=[0,0,0,1,1,1,2,2,2]
+            arry=[0,1,2,0,1,2,0,1,2]
+            for x in range(9):
+                row=i+arrx[x]
+                col=j+arry[x]
+                if board[row][col]=='.':
+                    continue
+                val= board[row][col]
+                if val in s:
+                    return False
+                s.add(val)
+            return True
+        for j in range(n):
+            S=set()
+            for i in range(m):
+                if board[i][j]=='.':
+                        continue
+                val=board[i][j]
+                if val in S:
+                    return False
+                S.add(val)
+        for i in range(m):
+            S=set()
+            for j in range(n):
+                if board[i][j]=='.':
+                        continue
+                val=board[i][j]
+                if val in S:
+                    return False
+                S.add(val)
+        for i in range(0,m,3):
+            for j in range(0,n,3):     
+                if not func(board,i,j):
+                    return False
+        return True
