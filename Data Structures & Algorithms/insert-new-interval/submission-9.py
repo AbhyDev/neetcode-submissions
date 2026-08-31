30cf0d9cea6df0f15a90116e1c4b3791d2014dc6
AbhyuDev <153921525+AbhyDev@users.noreply.github.com>
2026-09-01 04:01:38 +0530

    Add: insert-new-interval - submission-9

---
+++ Data Structures & Algorithms/insert-new-interval/submission-9.py
@@ -0,0 +1,32 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        n=len(intervals)
+        L=newInterval[0]
+        R=newInterval[1]
+        for i in range(n):
+            left=intervals[i][0]
+            right=intervals[i][1]
+            if L<left:
+                intervals.insert(i,[L,R])
+                break
+            elif L==left:
+                if R>=right:
+                    intervals.insert(i+1,[L,R])
+                else:
+                    intervals.insert(i,[L,R])
+        if len(intervals)==n:
+            intervals.append(newInterval)
+        ans=[]
+        left=intervals[0][0]
+        right=intervals[0][1]
+        for i in range(n+1):
+            l=intervals[i][0]
+            r=intervals[i][1]
+            if right>=l:
+                right=max(right,r)
+            else:
+                ans.append([left,right])
+                left=l
+                right=r
+        ans.append([left,right])
+        return ans
